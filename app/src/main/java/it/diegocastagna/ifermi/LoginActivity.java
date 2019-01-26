5a91e64e8eaf237e071c1a28e1e9be03fa3b62da
UnwishingMoon <11472973+UnwishingMoon@users.noreply.github.com>
2019-01-26 12:55:49 +0100

    TextView and Logo button

--- app/src/main/java/it/diegocastagna/ifermi/LoginActivity.java
+++ app/src/main/java/it/diegocastagna/ifermi/LoginActivity.java
@@ -6,7 +6,6 @@ import android.annotation.TargetApi;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
 import android.content.CursorLoader;
@@ -18,6 +17,7 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
