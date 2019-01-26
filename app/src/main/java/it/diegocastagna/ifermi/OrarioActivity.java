f0f086468a78d65bb04011817917bf3122296093
UnwishingMoon <11472973+UnwishingMoon@users.noreply.github.com>
2019-01-26 12:39:09 +0100

    Creation of all the needed Activity

---
+++ app/src/main/java/it/diegocastagna/ifermi/OrarioActivity.java
@@ -0,0 +1,13 @@
+package it.diegocastagna.ifermi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OrarioActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orario);
+    }
+}
