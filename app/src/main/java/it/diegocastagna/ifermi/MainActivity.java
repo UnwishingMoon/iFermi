93896955266d008d408346cca43487fb62ef5119
UnwishingMoon <11472973+UnwishingMoon@users.noreply.github.com>
2019-03-30 12:48:14 +0100

    Added Welcome and User MSG Added ScrollView

--- app/src/main/java/it/diegocastagna/ifermi/MainActivity.java
+++ app/src/main/java/it/diegocastagna/ifermi/MainActivity.java
@@ -2,6 +2,8 @@ package it.diegocastagna.ifermi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openDialer(View v){
+        // It opens the Dialer with the School Phone already typed in
     }
 }
