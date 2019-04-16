21fffd6f8acee609c4e7e05c1ac2abcafc599c45
UnwishingMoon <11472973+UnwishingMoon@users.noreply.github.com>
2019-04-16 08:13:09 +0200

    Some added

--- app/src/main/java/it/diegocastagna/ifermi/MainActivity.java
+++ app/src/main/java/it/diegocastagna/ifermi/MainActivity.java
@@ -10,13 +10,14 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
     private DrawerLayout drawerLayout; // Drawer of the Entire Activity
+    private String schoolPhone = "+39 0376 262675";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        drawerLayout = findViewById(R.id.drawer_layout); // Create the Drawer Layour for the Menu and the main content
+        this.drawerLayout = findViewById(R.id.drawer_layout); // Create the Drawer Layour for the Menu and the main content
 
         // TODO Retrieve user name from Files(?)
         /*TextView txt_view = (TextView) findViewById(R.id.msg_user);
@@ -24,20 +25,19 @@ public class MainActivity extends Activity {
         txt_view.setVisibility(View.VISIBLE); // Set it Visible*/
     }
 
-    public void openIntent(View v){
+    public void openGeoIntent(View v){
         Intent intent;
         Uri uri;
 
         // If the action is the address map
-        if(v.getId() == R.id.school_address){
-            try{
+        if(v.getId() == R.id.school_address) {
+            try {
                 uri = Uri.parse("geo:45.1412746,10.7645877,17z?q=" + Uri.encode("Istituto Enrico Fermi Mantova"));
                 intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
-            }catch(Exception e){
+            } catch (Exception e) {
 
             }
         }
-        // It opens the Dialer with the School Phone already typed in
     }
 }
