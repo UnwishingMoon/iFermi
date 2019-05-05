76b7e2dce64de69c959c54b3a8d317b2e187d5dd
UnwishingMoon <1472973+UnwishingMoon@users.noreply.github.com>
2019-05-05 20:43:58 +0200

    Implementing Model

--- app/src/main/java/it/diegocastagna/ifermi/Model.java
+++ app/src/main/java/it/diegocastagna/ifermi/Model.java
@@ -1,10 +1,22 @@
 package it.diegocastagna.ifermi;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
+import java.util.concurrent.ExecutionException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class Model extends Observable {
     public final static String rssUrl = "https://www.fermimn.edu.it/?action=rss";
@@ -12,10 +24,10 @@ public class Model extends Observable {
 
     private WebsiteCheck wCheck;
 
-    private String newsCacheFile = "newsCacheFile";
-    // private xmlTree;
+    private String newsCacheFile = "newsCacheFile.rss";
+    private List rssNews;
 
-    private String classesCacheFile = "classesCacheFile;";
+    private String classesCacheFile = "classesCacheFile.html";
     private List classesList;
 
     private String schoolClass;
@@ -30,26 +42,86 @@ public class Model extends Observable {
     }
 
     private Model(){
+        this.rssNews = new ArrayList();
+
         this.classesList = new ArrayList();
         this.schoolClass = "1A";
+
         this.wCheck = new WebsiteCheck();
     }
 
 
-    public void getNewsList(){
-        // Checking if Cache file Exists and is not empty
-        File f = new File(getCacheDir(), "newsCacheFile");
-        if(f.exists()){
+    public List getNewsList(){
+        // Check if Classes List exists
+        if(this.rssNews != null){
+            return rssNews;
+        }
 
+        // Checking if Cache file Exists and is not empty
+        File f = new File(newsCacheFile);
+        if(f.exists()){
+            try{
+                createRssNews(f);
+                return rssNews;
+            }catch (Exception e){
+                // Log the error
+                System.out.println("[ERROR]: During the creation of Rss News Tree in Memory");
+            }
+        } else {
+            boolean isInternetActive;
+            try{
+                isInternetActive = (Boolean) wCheck.execute().get(); // Wait to check if internet is up
+            }catch (ExecutionException e){
+                isInternetActive = false;
+            }catch (InterruptedException e){
+                isInternetActive = false;
+            }
+
+            // Retrieve the file from the website
+            if(isInternetActive) {
+                try {
+                    downloadFile(f, this.rssUrl);
+                    createRssNews(f);
+                } catch (Exception e) {
+                    // Log the error
+                    System.out.println("[ERROR]: During the download or creation of Rss News Tree in Memory");
+                }
+                return rssNews;
+            }
         }
-        if(this.classesFile == null){
-            instance = new Model();
+        return null;
+    }
+
+    private void createRssNews(File f) throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder parser =  factory.newDocumentBuilder();
+
+        Document doc = parser.parse(f); // XML Document
+        Element el = doc.getDocumentElement(); // Root Element
+        NodeList itemList = el.getElementsByTagName("item"); // List of all Items
+        for(int i = 0; i < itemList.getLength(); i++){
+            Node n = itemList.item(i);
         }
+    }
+
+    private void downloadFile(File f, String urlStr) throws IOException{
+        URL url = new URL(urlStr);
+        BufferedInputStream bis = new BufferedInputStream(url.openStream()); // Input Stream of the URL
+        FileOutputStream fos = new FileOutputStream(f); // Where the data will be saved
+
+        byte[] buffer = new byte[2048];
+        int count;
+        while((count = bis.read(buffer,0,2048)) != -1){
+            fos.write(buffer, 0, count);
+        }
+
+        fos.close();
+        bis.close();
     }
 
     public void getClassesList(){
         // Checking if Cache file Exists and is not empty
-        if(this.newsRssFile == null){
+        if(this.classesList != null){
             this.wCheck.execute();
         }
     }
