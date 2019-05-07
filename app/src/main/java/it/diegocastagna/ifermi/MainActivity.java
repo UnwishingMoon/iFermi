3d0ad68de75765fb834ad46c0ad1f25ccad355b9
UnwishingMoon <11472973+UnwishingMoon@users.noreply.github.com>
2019-05-07 18:39:42 +0200

    Created Toolbar

--- app/src/main/java/it/diegocastagna/ifermi/MainActivity.java
+++ app/src/main/java/it/diegocastagna/ifermi/MainActivity.java
@@ -1,10 +1,14 @@
 package it.diegocastagna.ifermi;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -13,29 +17,54 @@ import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
-public class MainActivity extends Activity implements Observer {
+public class MainActivity extends AppCompatActivity implements Observer {
     private Model mModel;
     private DrawerLayout drawerLayout; // Drawer of the Entire Activity
     private LinearLayout newsContainer;
     private TextView msgWelcome; // welcome user message that
+    private Toolbar topToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mModel = Model.getInstance();
-        mModel.setCacheDir(getCacheDir());
-        mModel.addObserver(this);
+        topToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(topToolbar);
 
         drawerLayout = findViewById(R.id.drawer_layout); // Create the Drawer Layour for the Menu and the main content
         msgWelcome = findViewById(R.id.msg_welcome); // TextView that will fade away after X seconds
         newsContainer = findViewById(R.id.news_container); // Linearlayout that should contain news
 
+        mModel = Model.getInstance();
+        mModel.setCacheDir(getCacheDir());
+        mModel.addObserver(this);
+
         List l = mModel.getNewsList();
         for(Object o : l){
             RssNews r = (RssNews) o;
 
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate( R.menu.menu_toolbar, menu );
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.toolbar_action_menu:
+                // User chose the "Menu" item, show the app Menu
+                drawerLayout.openDrawer(Gravity.LEFT);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
         }
     }
 
